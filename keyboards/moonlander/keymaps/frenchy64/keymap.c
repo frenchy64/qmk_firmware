abf55e5c354e777590bd82d6cd04314bbc949b07
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-11-27 17:18:20 -0500

    layer navigation + mouse key led

--- keyboards/moonlander/keymaps/frenchy64/keymap.c
+++ keyboards/moonlander/keymaps/frenchy64/keymap.c
@@ -90,7 +90,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,           TO(SYMB), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TG(MDIA),         TG(MDIA),  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
         KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   TO(BASE),           TO(MDIA), KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MDIA] = LAYOUT_moonlander(
-        LED_LEVEL,_______,   _______, _______, _______,    _______, _______,           _______, _______, _______, _______, _______, _______, RESET,
+        LED_LEVEL,_______,   _______, _______, _______,    _______, TO(SYMB),           TO(BASE), _______, _______, _______, _______, _______, RESET,
         _______, _______,    KC_WH_U, KC_MS_U, KC_WH_D,    _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______,    KC_MS_L, KC_MS_D, KC_MS_R,    _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
         _______, KC_MS_BTN1, _______, _______, _______,    _______,                             _______, _______, KC_MPRV, KC_MNXT, _______, _______,
@@ -156,88 +156,46 @@ void keyboard_post_init_user(void) {
                         32 33 34 70 69 68
 */
 
+void set_number_key_led_white(uint8_t layer_num) {
+  rgb_matrix_set_color_all(0, 0, 0);
+  switch (layer_num) {
+    case BASE:
+      // top left layer indicator
+      rgb_matrix_set_color(0, 0xff, 0xff, 0xff);
+      break;
+    case SYMB:
+      rgb_matrix_set_color(5, 0xff, 0xff, 0xff);
+      break;
+    case MDIA:
+      rgb_matrix_set_color(10, 0xff, 0xff, 0xff);
+      // mouse keys
+      rgb_matrix_set_color(16, 0xff, 0xff, 0xff);
+      rgb_matrix_set_color(12, 0xff, 0xff, 0xff);
+      rgb_matrix_set_color(17, 0xff, 0xff, 0xff);
+      rgb_matrix_set_color(22, 0xff, 0xff, 0xff);
+      break;
+    default:
+      break;
+  }
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
   //print debugging for current layer
 #ifdef CONSOLE_ENABLE
   switch(get_highest_layer(state)) {
     case BASE:
       dprintf("BASE layer\n");
-      for (uint8_t i = 0; i<DRIVER_LED_TOTAL; i++) {
-        switch(i%10) {
-          case 0:
-            rgb_matrix_set_color(i, 0, 0xff, 0);
-            break;
-          default:
-            switch(((i%10)+1)%5) {
-              case 0:
-                rgb_matrix_set_color(i, 0x80, 0, 0);
-                break;
-              case 1:
-                rgb_matrix_set_color(i, 0xff, 0, 0);
-                break;
-              case 2:
-                rgb_matrix_set_color(i, 0, 0, 0x80);
-                break;
-              case 3:
-                rgb_matrix_set_color(i, 0, 0, 0xff);
-                break;
-              case 4:
-                rgb_matrix_set_color(i, 0, 0xff, 0xff);
-                break;
-            }
-            break;
-        }
-      }
+      rgb_matrix_set_color_all(0, 0, 0);
+      // set the key left of 1
+      rgb_matrix_set_color(0, 0xff, 0xff, 0xff);
       break;
     case SYMB:
       dprintf("SYMB layer\n");
-      for (uint8_t i = 0; i<DRIVER_LED_TOTAL; i++) {
-        switch(i) {
-          case 29:
-            rgb_matrix_set_color(i, 0xff, 0xff, 0xff);
-            break;
-          case 30:
-            rgb_matrix_set_color(i, 0x60, 0, 0);
-            break;
-          case 31:
-            rgb_matrix_set_color(i, 0xff, 0, 0);
-            break;
-          case 32:
-            rgb_matrix_set_color(i, 0, 0x60, 0);
-            break;
-          case 33:
-            rgb_matrix_set_color(i, 0, 0xff, 0);
-            break;
-          case 34:
-            rgb_matrix_set_color(i, 0, 0, 0x60);
-            break;
-          case 35:
-            rgb_matrix_set_color(i, 0, 0, 0xff);
-            break;
-          case 36:
-            rgb_matrix_set_color(i, 0, 0x60, 0x60);
-            break;
-          case 37:
-            rgb_matrix_set_color(i, 0, 0xff, 0xff);
-            break;
-          case 38:
-            rgb_matrix_set_color(i, 0x60, 0x60, 0);
-            break;
-          case 39:
-            rgb_matrix_set_color(i, 0xff, 0xff, 0);
-            break;
-          case 40:
-            rgb_matrix_set_color(i, 0xff, 0xff, 0xff);
-            break;
-          default:
-            rgb_matrix_set_color(i, 0, 0, 0);
-            break;
-        }
-      }
+      set_number_key_led_white(SYMB);
       break;
     case MDIA:
       dprintf("MDIA layer\n");
-      rgb_matrix_set_color_all(0, 0, 0xff);
+      set_number_key_led_white(MDIA);
       break;
     default:
       dprintf("Unknown layer\n");
