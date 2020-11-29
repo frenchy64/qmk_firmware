a0138a76177aa4ae8fb4c363761af73a2ada0f66
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-11-29 17:57:18 -0500

    rm combos

--- keyboards/moonlander/keymaps/frenchy64/keymap.c
+++ keyboards/moonlander/keymaps/frenchy64/keymap.c
@@ -32,82 +32,27 @@ enum custom_keycodes {
     VRSN = ML_SAFE_RANGE,
 };
 
-// combos
-
-enum combos {
-  SD_LPRN, // s+d => (
-  DG_RPRN, // d+g => )
-  XC_LBRC, // x+c => [
-  CV_RBRC, // c+v => ]
-  WE_LCBR, // w+e => {
-  ER_RCBR, // e+r => }
-  QW_OPEN_CLJ_SET, // q+w => #{
-  QS_OPEN_CLJ_FN,   // q+s => #(
-  KL_ONE_SHOT_SHIFT
-};
-
-const uint16_t PROGMEM sd_combo [] = { KC_S, KC_D, COMBO_END };
-const uint16_t PROGMEM dg_combo [] = { KC_D, KC_G, COMBO_END };
-const uint16_t PROGMEM xc_combo [] = { KC_X, KC_C, COMBO_END };
-const uint16_t PROGMEM cv_combo [] = { KC_C, KC_V, COMBO_END };
-const uint16_t PROGMEM we_combo [] = { KC_W, KC_E, COMBO_END };
-const uint16_t PROGMEM er_combo [] = { KC_E, KC_R, COMBO_END };
-const uint16_t PROGMEM qw_combo [] = { KC_Q, KC_W, COMBO_END };
-const uint16_t PROGMEM qs_combo [] = { KC_Q, KC_S, COMBO_END };
-const uint16_t PROGMEM kl_combo [] = { KC_K, KC_L, COMBO_END };
-
-combo_t key_combos[COMBO_COUNT] = {
-  [SD_LPRN] = COMBO(sd_combo, KC_LPRN),
-  [DG_RPRN] = COMBO(dg_combo, KC_RPRN),
-  [XC_LBRC] = COMBO(xc_combo, KC_LBRC),
-  [CV_RBRC] = COMBO(cv_combo, KC_RBRC),
-  [WE_LCBR] = COMBO(we_combo, KC_LCBR),
-  [ER_RCBR] = COMBO(er_combo, KC_RCBR),
-  [QW_OPEN_CLJ_SET] = COMBO_ACTION(qw_combo),
-  [QS_OPEN_CLJ_FN]  = COMBO_ACTION(qs_combo),
-  [KL_ONE_SHOT_SHIFT] = COMBO(kl_combo, OSM(MOD_LSFT))
-};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case QW_OPEN_CLJ_SET:
-      if (pressed) {
-        tap_code16(KC_HASH);
-        tap_code16(KC_LCBR);
-      }
-      break;
-    case QS_OPEN_CLJ_FN:
-      if (pressed) {
-        tap_code16(KC_HASH);
-        tap_code16(KC_LPRN);
-      }
-      break;
-  }
-}
-
-//end combos
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,           TO(SYMB), KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_A,    KC_S, LCTL_T(KC_D),LSFT_T(KC_F),KC_G,KC_BSPC,         TG(MDIA),KC_H, RSFT_T(KC_J),RCTL_T(KC_K),    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
-        KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-    LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT,  KC_RGHT, KC_LEAD,    KC_LEAD,   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            LCMD_T(KC_SPC),  KC_BSPC, KC_LGUI,           KC_LALT,  KC_ENT, KC_ESC),
+        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,         XXXXXXX, KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,         XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        XXXXXXX, KC_A,    LT(SYMB, KC_S), LCTL_T(KC_D),LSFT_T(KC_F),KC_G,KC_BSPC,         MO(MDIA),LT(MDIA,KC_H), RSFT_T(KC_J),RCTL_T(KC_K),LT(SYMB,KC_L),    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
+        KC_LSFT, KC_Z, KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
+    LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT,  KC_RGHT, KC_LEAD,    KC_LEAD,   KC_DOWN, KC_UP, KC_LBRC, KC_RBRC, MO(SYMB),
+                                            LCMD_T(KC_SPC),  KC_BSPC, KC_MEH,           KC_LALT,  KC_ENT, RCMD_T(KC_ESC)),
 
     [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   TO(BASE),           TO(MDIA), KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   TO(BASE),          _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EEP_RST, _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
+        _______, _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          KC_0, KC_DOT,  KC_0,    KC_EQL,  _______,
                                             RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
     ),
 
     [MDIA] = LAYOUT_moonlander(
-        LED_LEVEL,_______,   _______, _______, _______,    _______, TO(SYMB),           TO(BASE), _______, _______, _______, _______, _______, RESET,
+        LED_LEVEL,_______,   _______, _______, _______,    _______, TO(BASE),           TO(BASE), _______, _______, _______, _______, _______, RESET,
         _______, _______,    KC_WH_U, KC_MS_U, KC_WH_D,    _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______,    KC_MS_L, KC_MS_D, KC_MS_R,    _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
         _______, _______, _______, _______, _______,    _______,                             _______, _______, KC_MPRV, KC_MNXT, _______, _______,
@@ -221,9 +166,6 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_THREE_KEYS(KC_S, KC_P, KC_I) {
-      SEND_STRING("|");
-    }
     SEQ_THREE_KEYS(KC_S, KC_H, KC_A) {
       SEND_STRING("#");
     }
