42e1ab12cb608dffeb7510d1b72d3fb3645e6675
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-12-03 10:40:04 -0600

    {KC=>QK}_LEAD

--- keyboards/zsa/moonlander/keymaps/frenchy64/keymap.c
+++ keyboards/zsa/moonlander/keymaps/frenchy64/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,        XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
         XXXXXXX,KC_A, LT(SYMB, KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,XXXXXXX,   XXXXXXX,LT(MDIA,KC_H), LSFT_T(KC_J),LCTL_T(KC_K),LT(SYMB,KC_L),KC_SCLN,XXXXXXX,
         XXXXXXX, KC_Z, KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-    XXXXXXX,XXXXXXX,XXXXXXX,KC_LEFT,  KC_RGHT, KC_LEAD,                         XXXXXXX,   KC_DOWN, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,XXXXXXX,XXXXXXX,KC_LEFT,  KC_RGHT, QK_LEAD,                         XXXXXXX,   KC_DOWN, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
                                             LCMD_T(KC_SPC),  KC_BSPC, KC_MEH,           LOPT_T(LOPT(KC_SPC)),  KC_ENT, LCMD_T(KC_ESC)),
 
     [SYMB] = LAYOUT_moonlander(
